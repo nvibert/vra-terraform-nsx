d11a4ffae938103ed470f5ecfae70b232bff6e66
Nico Vibert <nvibert@vmware.com>
2020-09-21 13:30:51 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -6,18 +6,38 @@ variable "vmc_token" {
   description = "VMC Token"
 }
 
-variable "VMC_subnets" {
-  default = {
+variable "Subnet12" {
+  default = "12.12.12.0/24"
+}
 
-    Subnet12            = "12.12.12.0/24"
-    Subnet12gw          = "12.12.12.1/24"
-    Subnet12dhcp        = "12.12.12.100-12.12.12.200"
+variable "Subnet12gw" {
+  default = "12.12.12.1/24"
+}
 
-    Subnet13            = "13.13.13.0/24"
-    Subnet13gw          = "13.13.13.1/24"
-    Subnet13dhcp        = "13.13.13.100-13.13.13.200"
+variable "Subnet12dhcp" {
+  default = "12.12.12.100-12.12.12.200"
+}
 
-    Subnet14            = "14.14.14.0/24"
-    Subnet14gw          = "14.14.14.1/24"
-  }
-  }
+variable "Subnet13" {
+  default = "13.13.13.0/24"
+}
+
+variable "Subnet13gw" {
+  default = "13.13.13.1/24"
+}
+
+variable "Subnet13dhcp" {
+  default = "13.13.13.100-13.13.13.200"
+}
+
+variable "Subnet14" {
+  default = "14.14.14.0/24"
+}
+
+variable "Subnet14gw" {
+  default = "14.14.14.1/24"
+}
+
+variable "Photo_IP" {
+  default = "18.133.44.151"
+}
